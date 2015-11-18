a662133892c4f5a7886faf3235a67f0ab900f160
Kun Ren <renkun@outlook.com>
2015-11-18 14:07:03 +0800

    make server command functions closures

--- R/server.R
+++ R/server.R
@@ -1,24 +1,23 @@
-server_eval <- function(expr) {
-  withVisible(eval(expr, globalenv()))
-}
-
-server_send <- function(expr) {
-  eval(expr, globalenv())
-  NULL
-}
-
-server_assign <- function(symbol, value) {
-  assign(symbol, value, globalenv())
-  NULL
-}
-
-server_kill <- function(...) {
-  closeAllConnections()
-  q(...)
-}
-
 #' @export
 sessionServer <- function(host = "localhost", port = 6001) {
+
+  # server commands
+  server_eval <- function(expr) {
+    withVisible(eval(expr, globalenv()))
+  }
+  server_send <- function(expr) {
+    eval(expr, globalenv())
+    NULL
+  }
+  server_assign <- function(symbol, value) {
+    assign(symbol, value, globalenv())
+    NULL
+  }
+  server_kill <- function(...) {
+    closeAllConnections()
+    q(...)
+  }
+
   cat("R Server Terminal\n")
   while (TRUE) {
     con <- try(socketConnection(host = host, port = port,
@@ -30,7 +29,9 @@ sessionServer <- function(host = "localhost", port = 6001) {
         request <- unserialize(con)
         cat(sprintf("[%s] %s > %s\n",
           format(request$time, "%Y-%m-%d %H:%M:%S"), request$client, request$command))
-        res <- try(do.call(sprintf("server_%s", request$command),
+        f <- get(sprintf("server_%s", request$command), environment(),
+          mode = "function", inherits = FALSE)
+        res <- try(do.call(f,
           request$args, quote = TRUE, envir = globalenv()), silent = TRUE)
         serialize(res, con)
       }, error = function(e) message(e, "\n"),
